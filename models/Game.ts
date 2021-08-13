34f3887bbf5ecacde2fdcd2c1d6862a7118ff9b1
Finley Sherwood <me@itsf.in>
2021-08-13 15:03:15 +1000

    Set number distance from bomb to tiles

--- models/Game.ts
+++ models/Game.ts
@@ -1,5 +1,7 @@
+export type Tile = number | 'BOMB'
+
 export default class Game {
-    public state: number[]
+    public state: Tile[]
     public width: number
     public height: number
 
@@ -14,14 +16,41 @@ export default class Game {
 
     private create(bombs: number): void {
         let bombIndex = new Set<number>()
-        let state = new Array<number>(this.width*this.height)
+        let state = new Array<Tile>(this.width*this.height)
 
         while (bombIndex.size < bombs) {
             let i = Math.round(Math.random()*this.width*this.height)
             bombIndex.add(i)
-            state[i] = 0
+            state[i] = 'BOMB'
         }
 
         this.state = state
+
+        for (let i = 0; i < this.state.length; i++) {
+            this.state[i] = this.bombsNearIndex(i)
+        }
+    }
+
+    private bombsNearIndex(index: number): Tile {
+        if (this.state[index] === 'BOMB') return 'BOMB'
+        
+        let relativePosition = [
+            -this.width - 1,    // top left
+            -this.width,        // top
+            -this.width + 1,    // top right
+            -1,                 // left
+            1,                  // right
+            this.width - 1,     // bottom left
+            this.width,         // bottom
+            this.width + 1      // bottom right
+        ]
+
+        let bombsNearby = 0
+
+        for (let position of relativePosition) {
+            if (this.state[index + position] === 'BOMB') bombsNearby++
+        }
+
+        return bombsNearby
     }
 }
